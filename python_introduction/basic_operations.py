5a6495f6259121e0032d296fe52578964a9a082f
Tunde <tundeprince050@gmail.com>
2025-11-10 21:34:36 +0000

    First Python Task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+diference = number1 - number2
+product = number1 * number2
+print ( 'Addition of',number1,"and",number2,"is",sum,
+    'Substraction of',number1,'and', number2,"is",diference,
+    'Multiplication of',number1,'and',number2,'is',product)
