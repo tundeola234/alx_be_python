9c4a6c16b14781d8beb9088f5d5c457c7b8b4054
tundeola234 <117745897+tundeola234@users.noreply.github.com>
2025-11-16 14:29:46 +0000

    Modify calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operation = input("Choose an operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /):").strip()
 
 match operation:
     case "+":
