327a4bbca112d756bc2f07bd27fce058df4f8721
tundeola234 <117745897+tundeola234@users.noreply.github.com>
2025-11-30 21:59:41 +0000

    Modify Bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}.00")
