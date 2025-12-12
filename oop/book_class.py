10eb8f32d6d12f6b4639cfa033f45ce2a97b2ae3
Prince Tunde Gbadmosi <ptg@oamarkets.local>
2025-12-12 14:09:59 +0000

    Python Magic Methods

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book():
+    def __init__(self, title, author, publication_year):
+        self.title = str(title)
+        self.author = str(author)
+        self.year = int(publication_year)
+    
+    def __del__(self):
+        self.title.close
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+    
+
+    
