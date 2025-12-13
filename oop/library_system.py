8ac07753918912cfc71a2a2a4f8af0e33b1f182b
Prince Tunde Gbadmosi <ptg@oamarkets.local>
2025-12-13 14:27:35 +0000

    Add Library Sytem

---
+++ oop/library_system.py
@@ -0,0 +1,38 @@
+#Base Class- Book
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def get_info(self):
+        return f"Book: {self.title} by {self.author}"
+
+#Derived Classes - EBook
+class EBook(Book):
+    def __init__(self, title:str, author :str, file_size : int):
+        super().__init_(title, author)
+        self.file_size = file_size
+        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}"
+       
+#Derived Classes - PrintBook
+class PrintBook(Book):
+    def __init_(self, title:str, author :str, page_count : int):
+        super().__init_(title, author)
+        self.page_count = page_count
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+    
+#Composition - Library
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+    
+    def list_books(self):
+        for i in self.books:
+            print(i)
+
+    
+    
